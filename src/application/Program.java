b22d750655744b96e23f12a543c80be672ec4529
Hiran Ferreti Baccos <hiran.baccos.prestserv@petrobras.com.br>
2026-01-21 18:54:37 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+
+		sc.close();
+	}
+}
